3613ba18c811d808ed42888ddab166698de9a23b
Thiago Cabral Correia <thiagocabralcorreia@gmail.com>
2022-05-05 13:30:30 -0300

    feature: add feedback steps

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,4 +1,8 @@
-import {CloseButton} from '../CloseButton';
+import { useState } from 'react';
+
+import { FeedbackTypeStep} from './Steps/FeedbackTypeStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSucessStep } from './Steps/FeedbackSucessStep';
 
 import bugImg from '../../assets/bug.svg';
 import lampImg from '../../assets/lamp.svg';
@@ -6,53 +10,63 @@ import thoughtImg from '../../assets/thought.svg';
 
 export const feedbackTypes = {
     BUG: {
-        title: 'Bug', // 'Report a problem'
+        title: 'Bug',
         image: {
             source:bugImg,
             alt: 'Bug image',
-            description: 'Report a problem'
+            description: 'Report a problem',
+            placeholder: 'Report a problem. Describe the issue you\'re experiencing.'
         }
     },
     IDEA: {
-        title: 'Idea', // 'Submit a suggestion'
+        title: 'Idea',
         image: {
             source: lampImg,
             alt: 'Lamp image',
-            description: 'Submit a suggestion'
+            description: 'Submit a suggestion',
+            placeholder: 'Your idea counts. Submit a suggestion.'
         }
     },
     OTHER: {
-        title: 'Others', // 'Compliments, complaints and questions'
+        title: 'Others', //
         image: {
             source: thoughtImg,
             alt: 'Thought image',
-            description: 'Compliments, complaints and questions'
+            description: 'Compliments, complaints and questions',
+            placeholder: 'Write your compliments, complaints and questions here.'
         }
     }
 }
 
-export function WidgetForm() {
-  return (
-    <div className='bg-zinc-900 p-8 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
-      <header>
-          <span className='text-xl leading-6'>We want your feedback</span>
-          <CloseButton />
-      </header>
-      <div className="flex py-8 gap-2 w-full">
-        { Object.entries(feedbackTypes).map(([key, value]) => {
-            return(
-                <button
-                    key={key}
-                    className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-row items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                    type="button"
-                    title={value.image.description}
-                >
-                    <img src={value.image.source} alt={value.image.alt} className="max-h-8 max-w-5 mx-auto mb-2"/>
-                    <span>{value.title}</span>
-                </button>
-            )
-        }) }
+export type FeedBackType = keyof typeof feedbackTypes;
+
+export function WidgetForm(){
+    const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleRestartFeedback(){
+        setFeedbackSent(false);
+        setFeedbackType(null);
+    }
+    
+    return(
+        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
+            { feedbackSent ? (
+                <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback} />
+            ) : (
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep 
+                            feedbackType={feedbackType} 
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+                )
+            }
         </div>
-    </div>
-  )
+    )
 }
